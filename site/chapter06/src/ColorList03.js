c7e406939776c9474a5a7cbf644a9320d0f137c5
firmansiahaan <firman.siahaan@hotmail.com>
2026-06-02 05:48:28 +0700

    update

---
+++ site/chapter06/src/ColorList03.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Color from "./Color.js";
+
+export default function ColorList({ colors = [], onRemoveColor = f => f,
+    onRateColor = f => f
+ }) {
+    if(!colors.length) return <div>No Colors Listed. (Add a Color)</div>;
+    return (
+        <div>
+        {
+            colors.map(color => <Color key={color.id} {...color} 
+                onRemove={onRemoveColor} onRate={onRateColor} />)
+        }
+        </div>
+    );
+}
