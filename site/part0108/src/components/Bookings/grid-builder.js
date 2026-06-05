88628e2ff5535c9586ad650a2378c524f80203ec
firmansiahaan <firman.siahaan@hotmail.com>
2026-06-06 03:32:54 +0700

    update

---
+++ site/part0108/src/components/Bookings/grid-builder.js
@@ -0,0 +1,44 @@
+import session_data from "../../static.json";
+import {addDays, shortISO} from "../../utils/date-wrangler";
+
+export function getGrid (bookable, startDate) {
+
+  const dates = bookable.days.sort().map(
+    d => shortISO(addDays(startDate, d))
+  );
+
+  const sessions = bookable.sessions.map(i => session_data.sessions[i]);
+
+  const grid = {};
+
+  sessions.forEach(session => {
+    grid[session] = {};
+    dates.forEach(date => grid[session][date] = {
+      session,
+      date,
+      bookableId: bookable.id,
+      title: ""
+    });
+  });
+
+  return {
+    grid,
+    dates,
+    sessions
+  };
+}
+
+export function transformBookings (bookingsArray) {
+  return bookingsArray.reduce((bookings, booking) => {
+
+    const {session, date} = booking;
+
+    if (!bookings[session]) {
+      bookings[session] = {};
+    }
+
+    bookings[session][date] = booking;
+
+    return bookings;
+  }, {});
+}
