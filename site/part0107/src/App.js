499befe74882788520e0ddcda93ef10355d24fa1
firmansiahaan <firman.siahaan@hotmail.com>
2026-06-06 01:56:03 +0700

    update

---
+++ site/part0107/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AnagramsMain from "./anagrams-correct";
+
+function App() {
+  return (
+    <div className="App">
+      <AnagramsMain />
+    </div>
+  );
+}
+
+export default App;
