ae41837b06108aa8c902e305bc34d356b57eb71c
firmansiahaan <firman.siahaan@hotmail.com>
2026-06-08 19:29:36 +0700

    update

---
+++ site/A-digital-ocean-tutorial/how08-hooks-usestate/src/components/Product/Product.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import './Product.css';
+
+const products = [
+    {
+        emoji: '🍦',
+        name: 'ice cream',
+        price: 5
+    },
+    {
+        emoji: '🍩',
+        name: 'donuts',
+        price: 2.5,
+    },
+    {
+        emoji: '🍉',
+        name: 'watermelon',
+        price: 4
+    }
+];
+
+const currencyOptions = {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+}
+
+function getTotal(total) {
+    return total.toLocaleString(undefined, currencyOptions);
+}
+
+export default function Product() {
+    const [cart, setCart] = useState([]);
+    const [total, setTotal] = useState(0);
+
+    function add(product) {
+        setCart(current => [...current, product.name]);
+        setTotal(current => current + product.price);
+    }
+
+    return (
+        <div className="wrapper">
+            <div>
+                Shopping Cart: {cart.length} total items.
+            </div>
+            <div>Total: {getTotal(total)}</div>
+
+            <div>
+                {products.map((product) => (
+                    <div key={product.name}>
+                        <div className="product">
+                            <span role="img" aria-label="ice-cream">{product.emoji}</span>
+                        </div>
+                        <button onClick={() => add(product)}>Add</button>&nbsp;
+                        <button onClick={() => {
+                            setCart([]);
+                            setTotal(0);
+                        }}>
+                            Remove
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
