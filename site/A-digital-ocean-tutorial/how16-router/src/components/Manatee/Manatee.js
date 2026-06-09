290cd013b652abce64f7f76f41a206198d8fbf09
firmansiahaan <firman.siahaan@hotmail.com>
2026-06-09 22:38:15 +0700

    update

---
+++ site/A-digital-ocean-tutorial/how16-router/src/components/Manatee/Manatee.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Manatee() {
+    return (
+        <React.Fragment>
+            <h2>Manatee</h2>;
+        </React.Fragment>
+    )
+}
---
+++ site/A-digital-ocean-tutorial/how16-router/src/components/Narwhal/Narwhal.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Narwhal() {
+    return (
+        <React.Fragment>
+            <h2>Narwhal</h2>;
+        </React.Fragment>
+    )
+}
