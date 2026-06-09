41e58525f961fe46518c641e1ad16df05331b0f2
firmansiahaan <firman.siahaan@hotmail.com>
2026-06-10 01:36:24 +0700

    update

---
+++ site/A-digital-ocean-tutorial/how19-deploy/src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, world!</h1>
+      <h2>Powered by ReactJs</h2>
+    </div>
+  );
+}
+
+export default App;
