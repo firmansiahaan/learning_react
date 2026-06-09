676eff60f227955308a9748233a67d1ecdefb59a
firmansiahaan <firman.siahaan@hotmail.com>
2026-06-10 00:23:42 +0700

    update

--- site/A-digital-ocean-tutorial/how18-performance/src/components/App/App.js
+++ site/A-digital-ocean-tutorial/how18-performance/src/components/App/App.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useCallback, useReducer, useState } from 'react';
 import './App.css';
 
+import CharacterMap from '../CharacterMap/CharacterMap';
+
 function App() {
+  const [text, setText] = useState('');
+  const [showExplanation, toggleExplanation] = useReducer(state => !state, false);
+  const transformer = useCallback(item => item.toLowerCase(), []);
+  
   return (
-    <div className="App">
-      
+    <div className="wrapper">
+      <label htmlFor="text">
+        <p>Add Your Text Here:</p>
+        <textarea
+          id="text"
+          name="text"
+          rows="10"
+          cols="100"
+          onChange={event => setText(event.target.value)}
+        >
+        </textarea>
+      </label>
+      <div>
+        <button onClick={toggleExplanation}>Show Explanation</button>
+      </div>
+      {showExplanation &&
+        <p>
+          This displays a list of the most common characters.
+        </p>
+      }
+      <CharacterMap showExplanation={showExplanation} text={text}
+        transformer={transformer} />
     </div>
   );
 }
