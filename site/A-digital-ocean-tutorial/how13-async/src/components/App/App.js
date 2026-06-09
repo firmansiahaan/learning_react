e08447424302e04abf1424ba31f12bb109201fb7
firmansiahaan <firman.siahaan@hotmail.com>
2026-06-09 15:51:39 +0700

    update

---
+++ site/A-digital-ocean-tutorial/how13-async/src/components/App/App.js
@@ -0,0 +1,27 @@
+import React, { lazy, Suspense, useReducer, useState } from 'react';
+import PropTypes from 'prop-types';
+import './App.css';
+const RiverInformation = lazy(() => import(/* webpackChunkName: "RiverInformation" */ '../RiverInformation/RiverInformation'));
+
+function App() {
+  const [river, setRiver] = useState('nile');
+  const [show, toggle] = useReducer(state => !state, true);
+
+  return (
+    <div className="wrapper">
+      <h1>World's Longest Rivers</h1>
+      <div><button onClick={toggle}>Toggle Details</button></div>
+      <button onClick={() => setRiver('nile')}>Nile</button>
+      <button onClick={() => setRiver('amazon')}>Amazon</button>
+      <button onClick={() => setRiver('yangtze')}>Yangtze</button>
+      <button onClick={() => setRiver('mississippi')}>Mississipi</button>
+      {show && (
+        <Suspense fallback={<div>Loading...</div>}>
+          <RiverInformation name={river} />
+        </Suspense>
+      )}
+    </div>
+  );
+}
+
+export default App;
