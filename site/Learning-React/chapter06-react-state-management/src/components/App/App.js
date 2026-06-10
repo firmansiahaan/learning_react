99b68592648ecf18f766cca5160570188b8ce8c8
firmansiahaan <firman.siahaan@hotmail.com>
2026-06-10 20:59:26 +0700

    update

--- site/Learning-React/chapter06-react-state-management/src/components/App/App.js
+++ site/Learning-React/chapter06-react-state-management/src/components/App/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
+import colorData from "../../color-data.json";
+import ColorList from "../ColorList/ColorList";
 import './App.css';
 
 function App() {
+  const [colors, setColors] = useState(colorData);
+
   return (
     <div className="App">
-      
+      <ColorList colors={colors} 
+        onRemoveColor={id => {
+          const newColors = colors.filter(color => color.id !== id);
+          setColors(newColors);
+        }}
+        onRateColor={(id, rating) => {
+          const newColors = colors.map(color =>
+            color.id === id ? { ...color, rating } : color
+          );
+          setColors(newColors)
+        }}
+      />
     </div>
   );
 }
